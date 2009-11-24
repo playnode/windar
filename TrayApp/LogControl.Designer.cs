59b1805c861aebdad13b1c65f082d4beba198c66
Steven Robertson <steverobertson@gmail.com>
2009-11-24 23:31:18 +0000

    Adding context menu to log tab.

---
+++ TrayApp/LogControl.Designer.cs
@@ -0,0 +1,130 @@
+﻿/*
+ * Windar: Playdar for Windows
+ * Copyright (C) 2009 Steven Robertson <steve@playnode.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Windar.TrayApp
+{
+    partial class LogControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.logBox = new System.Windows.Forms.RichTextBox();
+            this.LogContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.selectAllContextMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyContextMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.clearContextMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LogContextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // logBox
+            // 
+            this.logBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.logBox.ContextMenuStrip = this.LogContextMenu;
+            this.logBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logBox.Location = new System.Drawing.Point(0, 0);
+            this.logBox.Name = "logBox";
+            this.logBox.ReadOnly = true;
+            this.logBox.Size = new System.Drawing.Size(606, 401);
+            this.logBox.TabIndex = 1;
+            this.logBox.Text = "";
+            this.logBox.WordWrap = false;
+            // 
+            // LogContextMenu
+            // 
+            this.LogContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.selectAllContextMenuItem,
+            this.copyContextMenuItem,
+            this.toolStripSeparator1,
+            this.clearContextMenuItem});
+            this.LogContextMenu.Name = "LogContextMenu";
+            this.LogContextMenu.Size = new System.Drawing.Size(153, 98);
+            // 
+            // selectAllContextMenuItem
+            // 
+            this.selectAllContextMenuItem.Name = "selectAllContextMenuItem";
+            this.selectAllContextMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.selectAllContextMenuItem.Text = "Select All";
+            this.selectAllContextMenuItem.Click += new System.EventHandler(this.selectAllContextMenuItem_Click);
+            // 
+            // copyContextMenuItem
+            // 
+            this.copyContextMenuItem.Name = "copyContextMenuItem";
+            this.copyContextMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.copyContextMenuItem.Text = "Copy";
+            this.copyContextMenuItem.Click += new System.EventHandler(this.copyContextMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // clearContextMenuItem
+            // 
+            this.clearContextMenuItem.Name = "clearContextMenuItem";
+            this.clearContextMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.clearContextMenuItem.Text = "Clear";
+            this.clearContextMenuItem.Click += new System.EventHandler(this.clearContextMenuItem_Click);
+            // 
+            // LogControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.logBox);
+            this.Name = "LogControl";
+            this.Size = new System.Drawing.Size(606, 401);
+            this.Load += new System.EventHandler(this.Log_Load);
+            this.LogContextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox logBox;
+        private System.Windows.Forms.ContextMenuStrip LogContextMenu;
+        private System.Windows.Forms.ToolStripMenuItem selectAllContextMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyContextMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem clearContextMenuItem;
+    }
+}
---
+++ TrayApp/LogControl.cs
@@ -0,0 +1,142 @@
+﻿/*
+ * Windar: Playdar for Windows
+ * Copyright (C) 2009 Steven Robertson <steve@playnode.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using log4net;
+using log4net.Appender;
+
+namespace Windar.TrayApp
+{
+    partial class LogControl : UserControl
+    {
+        //TODO: Update the log box periodically, limiting the contents too.
+        //TODO: Provide option to follow tail (or follow tail when scrollbar near end?)
+
+        #region Properties
+
+        public Timer Timer { get; private set; }
+
+        private RichTextBox _logBox;
+        private MemoryAppender _memoryAppender;
+
+        private RichTextBox LogBox
+        {
+            get
+            {
+                if (_logBox == null)
+                {
+                    var ctrl = Controls.Find("logBox", true);
+                    if (ctrl.Length <= 0) throw new ApplicationException("Didn't find logBox!");
+                    _logBox = (RichTextBox)ctrl[0];
+                }
+                return _logBox;
+            }
+        }
+
+        private MemoryAppender MemoryAppender
+        {
+            get
+            {
+                if (_memoryAppender == null)
+                {
+                    var appenders = LogManager.GetRepository().GetAppenders();
+                    foreach (var appender in appenders)
+                    {
+                        if (appender.Name != "MemoryAppender") continue;
+                        _memoryAppender = (MemoryAppender)appender;
+                        break;
+                    }
+                }
+                return _memoryAppender;
+            }
+        }
+
+        #endregion
+
+        public LogControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Log_Load(object sender, EventArgs e)
+        {
+            // Set the log box font.
+            const string preferredFontName = "ProFontWindows";
+            var testFont = new Font(preferredFontName, 12, FontStyle.Regular, GraphicsUnit.Pixel);
+            LogBox.Font = testFont.Name == preferredFontName ? testFont : new Font("Courier New", 13, FontStyle.Regular, GraphicsUnit.Pixel);
+
+            // Create timer for updating.
+            Timer = new Timer { Interval = 10 };
+            Timer.Tick += LogBoxTimer_Tick;
+        }
+
+        private void LogBoxTimer_Tick(object sender, EventArgs e)
+        {
+            UpdateLogBox();
+        }
+
+        public void ScrollToEnd()
+        {
+            LogBox.SelectionStart = LogBox.TextLength;
+            LogBox.ScrollToCaret();
+        }
+
+        private void UpdateLogBox()
+        {
+            var events = MemoryAppender.GetEvents();
+            if (events.Length <= 0) return;
+            var sb = new StringBuilder();
+            sb.Append(LogBox.Text);
+            foreach (var logEvent in events)
+            {
+                var msg = logEvent.RenderedMessage;
+                if (msg.StartsWith("CMD.INF: "))
+                {
+                    sb.Append(msg.Substring(9, msg.Length - 9));
+                    sb.Append('\n');
+                }
+                else if (msg.StartsWith("CMD.ERR: "))
+                {
+                    sb.Append("ERROR! ").Append(msg.Substring(9, msg.Length - 9));
+                    sb.Append('\n');
+                }
+            }
+            MemoryAppender.Clear();
+            LogBox.Text = sb.ToString();
+            ScrollToEnd();
+        }
+
+        private void selectAllContextMenuItem_Click(object sender, EventArgs e)
+        {
+            LogBox.SelectAll();
+        }
+
+        private void copyContextMenuItem_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(LogBox.SelectedText);
+        }
+
+        private void clearContextMenuItem_Click(object sender, EventArgs e)
+        {
+            LogBox.Clear();
+        }
+    }
+}
