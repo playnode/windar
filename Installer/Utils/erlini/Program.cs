a9a9db9b9242235a3ec1e4667a853e90b05bacfe
Steven Robertson <steve@playnode.org>
2009-12-22 14:06:46 +0000

    Updating GPL file headers.

--- Installer/Utils/erlini/Program.cs
+++ Installer/Utils/erlini/Program.cs
@@ -1,20 +1,23 @@
-﻿/*
- * Windar: Playdar for Windows
- * Copyright (C) 2009 Steven Robertson <steve.r@k-os.net>
+﻿/*************************************************************************
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * This program is free software: you can redistribute it and/or modify
+ * Copyright (C) 2009 Steven Robertson <steve@playnode.org>
+ *
+ * Windar: Playdar for Windows
+ *
+ * Windar is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Windar is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+ ************************************************************************/
 
 using System;
 using System.IO;
--- TrayApp/OutputDisplay.Designer.cs
+++ TrayApp/OutputDisplay.Designer.cs
@@ -1,20 +1,23 @@
-﻿/*
- * Windar: Playdar for Windows
+﻿/*************************************************************************
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
  * Copyright (C) 2009 Steven Robertson <steve@playnode.org>
  *
- * This program is free software: you can redistribute it and/or modify
+ * Windar: Playdar for Windows
+ *
+ * Windar is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Windar is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+ ************************************************************************/
 
 namespace Windar.TrayApp
 {
