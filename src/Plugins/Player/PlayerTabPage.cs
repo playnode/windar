67681fd424d7ba4edd36c6390bbcd3c1eb06e6d4
Steven Robertson <steverobertson@gmail.com>
2018-09-30 09:49:37 +0100

    Removed the existing LGPL copyright headers

--- src/Plugins/Player/PlayerTabPage.cs
+++ src/Plugins/Player/PlayerTabPage.cs
@@ -1,26 +1,4 @@
-﻿/*
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * Copyright (C) 2009, 2010, 2011 Steven Robertson <steve@playnode.com>
- *
- * Windar - Playdar for Windows
- *
- * Windar is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License (LGPL) as published
- * by the Free Software Foundation; either version 2.1 of the License, or (at
- * your option) any later version.
- *
- * Windar is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License version 2.1 for more details
- * (a copy is included in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-using System;
+﻿using System;
 using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
